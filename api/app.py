1ecd7bbf2c7854b6b9768c1fdcaddd1efb8abef6
Thiago Esterci Fernandes <thiago.esterci@engenharia.ufjf.br>
2023-10-20 14:34:18 -0300

    sem comentários

--- api/app.py
+++ api/app.py
@@ -1,17 +1,10 @@
-# Esse código é um exemplo de um aplicativo web em Python usando 
-# o framework Flask, que oferece uma API de classificação de dígitos. 
-
 from flask import Flask, jsonify
 from flask_restx import Api
 from flask_cors import CORS
 from resources.digit_predict import classification_model
 
-# Cria uma instância do aplicativo Flask.
 app = Flask(__name__)
 
-# Cria uma instância da classe Api passando o aplicativo Flask 
-# criado anteriormente. Define informações sobre a API, como título, 
-# versão e descrição, que são usadas para documentação.
 api = Api(
     app,
     title="API do workshop de integração com ML",
@@ -19,19 +12,9 @@ api = Api(
     description="API de classificação de dígitos",
 )
 
-# Configura a política de compartilhamento de recursos (CORS) para 
-# o aplicativo Flask. Isso permite que a API seja acessada a partir de
-#  diferentes origens, por exemplo, de um navegador web.
 cors = CORS(app)
 
-# Adiciona um namespace à API, que é definido pelo objeto classification_model.
-#  Isso vincula um conjunto de endpoints da API a esse namespace, que, 
-# neste caso, é relacionado à classificação de dígitos.
-api.add_namespace(classification_model, '/digit-predict')
+api.add_namespace(classification_model, "/digit-predict")
 
-#  Inicia o servidor web Flask. Ele escuta na interface "0.0.0.0" 
-# (o que significa que está disponível em todas as interfaces), na porta 5000, 
-# com depuração ativada. O argumento threaded=False significa que o aplicativo 
-# não deve ser executado em um ambiente com múltiplas threads.
-if __name__ == '__main__':
+if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000, debug=True, threaded=False)
--- api/resources/digit_predict.py
+++ api/resources/digit_predict.py
@@ -1,20 +1,11 @@
-# Importações das bibliotecas necessárias. Flask-Restx é usado para criar
-# a API REST. O Model é importado do módulo model, que deve conter a classe
-# de modelo discutida na pergunta anterior.
 from flask_restx import Resource, Namespace, reqparse, fields
 from model import Model
 
-# Criação de um namespace chamado classification_model com uma breve descrição.
-# Isso é usado para organizar os endpoints da API.
 classification_model = Namespace(
     "classification_model",
     "Endpiont para classifiicar os dígitos escritos a mão",
 )
 
-# Define um modelo de dados chamado modelo para a API. O modelo consiste em um 
-# campo chamado "pixel_values", que é uma string que representa os valores de 
-# pixels na imagem. É especificado que esse campo é obrigatório, tem um comprimento 
-# máximo de 600 caracteres e possui uma descrição.
 modelo = classification_model.model(
     "classification_model",
     {
@@ -26,40 +17,22 @@ modelo = classification_model.model(
     },
 )
 
-# Cria uma classe chamada predictionModel que herda de Resource. Essa classe representa 
-# o endpoint da API para previsão de modelos.
+
 @classification_model.route("")
 class predictionModel(Resource):
-    # Define um objeto atributos do tipo RequestParser para analisar os argumentos da 
-    # solicitação. Ele adiciona um argumento chamado "pixel_values" que é uma string e 
-    # é obrigatório.
     atributos = reqparse.RequestParser()
     atributos.add_argument("pixel_values", type=str, required=True)
-    
-    # Usa um decorador expect para definir que o método POST espera um modelo de dados 
-    # conforme definido em modelo. O parâmetro validate=True indica que a validação
-    #  deve ser realizada.
+
     @classification_model.expect(modelo, validate=True)
     def post(self):
-
-        # Analisa os argumentos da solicitação usando o RequestParser definido 
-        # anteriormente e armazena-os em dados.
         dados = predictionModel.atributos.parse_args()
 
-        # Cria uma instância da classe Model (presumivelmente, a mesma classe de 
-        # modelo discutida na primeira resposta).
         predict_model = Model()
 
-        # Divide a string de valores de pixel em uma lista e converte cada valor em 
-        # ponto flutuante.
         input_str = dados.pixel_values.split(",")
 
         input_dados = [float(str_value) for str_value in input_str]
 
-        # Faz uma chamada ao método predict do modelo para realizar a previsão com 
-        # base nos dados de entrada. Se a previsão for bem-sucedida, ela é retornada. 
-        # Se ocorrer uma exceção, uma resposta de erro é retornada com o código de
-        # status 500.
         try:
             prediction = predict_model.predict(input_dados)
 
